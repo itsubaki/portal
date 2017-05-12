f8311600be3761d58dea7a1ee8c3ab254b0d206b
tsubaki <tsubaki@moon>
2017-05-12 21:44:27 +0900

    Add door.ex

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,18 +1,40 @@
 defmodule Portal do
-  @moduledoc """
-  Documentation for Portal.
-  """
+  defstruct [:left, :right]
 
-  @doc """
-  Hello world.
+  def transfer(left, right, data) do
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
 
-  ## Examples
+    %Portal{left: left, right: right}
+  end
 
-      iex> Portal.hello
-      :world
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
+      :error   -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
 
-  """
-  def hello do
-    :world
+    portal
+  end
+end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door = inspect(left)
+    right_door = inspect(right)
+
+    left_data = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
+
   end
 end
